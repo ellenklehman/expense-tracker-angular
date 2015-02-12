c98a95a245b51f5dc6d26b16943dd0dc43ff3b97
Ellen Krause Lehman <student@epicodus.com>
2015-02-12 12:19:38 -0800

    expense tracker completed according to intro to angular lesson

---
+++ app.js
@@ -0,0 +1,1 @@
+var expenseTracker = angular.module('expenseTracker', []);
---
+++ js/ExpensesController.js
@@ -0,0 +1,11 @@
+expenseTracker.controller('ExpensesCtrl', function ExpensesCtrl($scope) {
+  $scope.expenses = [];
+  $scope.addExpense = function() {
+    $scope.expenses.push({ "name": $scope.expenseName });
+    $scope.expenseName = null;
+  };
+  $scope.deleteExpense = function(expense) {
+    var index = $scope.expenses.indexOf(expense)
+    $scope.expenses.splice(index, 1);
+  };
+});
