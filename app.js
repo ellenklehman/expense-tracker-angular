d7d936963a757a7b0c860217486638e438ab79cc
Ellen Krause Lehman <student@epicodus.com>
2015-02-12 17:42:31 -0800

    WIP trying to get category.id to work in categories.html

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 var expenseTracker = angular.module('expenseTracker', ['ui.router']);
+
+expenseTracker.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/categories");
+
+  $stateProvider
+    .state('categories', {
+      url: "/categories",
+      templateUrl: "partials/categories.html",
+      controller: function($scope) {
+        $scope.categories = ["Food", "Home", "Work"];
+      }
+    })
+
+    .state('categories.detail', {
+      url: "/:id",
+      templateUrl: "partials/categories.detail.html",
+      controller: function($scope, $stateParams) {
+        categoryId = parseInt($stateParams.id)
+        $scope.category = $scope.categories[categoryId]
+      }
+    })
+});
